503b1a0a8dfd4d8e1b3159578232e0d0606a038c
Rosamaria Rodriguez <rosamaria.developer@gmail.com>
2022-11-03 00:43:37 -0500

    CREATE: Footer Component with styles

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-auto">
+      <footer className="mb-0 bg-dark text-white-50 text-center">
+        <div className="container-lg">
+          <p className="mb-0 py-3">
+            &copy;Proyecto con fines educativos elaborados conjuntamente por:{" "}
+            <a
+              href="https://github.com/ahmedhabiib"
+              className="link text-decoration-none text-light"
+            >
+              Rosamaria Developer & Coderhouse
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
